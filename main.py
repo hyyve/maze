bec9be2233b76403a41d921e06b6bd5cc111ac31
hyyve <hyvent82@gmail.com>
2025-01-30 21:09:49 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,12 +1,12 @@
-import constant
-import Maze
-import MazeGenerator
+from maze_generator import MazeGenerator
+from maze import Maze
 
-def main():
-    x = int(input("Enter the width : "))
-    y= int(input("Enter the height : "))
-    maze = Maze.Maze(x,y)
-    print(maze.str())
+def main() -> None:
+    width, height = 20, 20
+    maze_generator = MazeGenerator(width, height)
+    grid, start, finish = maze_generator.generate()
+    maze = Maze(grid, start, finish)
+    print(maze)
 
 if __name__ == "__main__":
     main()
